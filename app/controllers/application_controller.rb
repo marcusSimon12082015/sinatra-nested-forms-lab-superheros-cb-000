54eea393695a787b7a2a9969b1173d4526606a6e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-23 21:40:38 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,13 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    get '/' do
+      erb :heroform
+    end
 
-
+    post '/teams' do
+      @team = params[:team]
+      binding.pry
+      erb :team
+    end
 end
